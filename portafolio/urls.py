16aef1e07bb9731f79cf4e40cce76b143c7d2e1b
Esteban Heredia <estebanheredia@msn.com>
2024-04-17 21:12:52 -0300

    Terminada toda la parte estatica del proyecto, agregago de plantillas y adaptacion

--- portafolio/urls.py
+++ portafolio/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core import views
 urlpatterns = [
+    path("contact/", views.contact, name="contact"),
+    path("portfolio/", views.portfolio, name="portfolio"),
+    path('about-me/', views.about, name="about-me"),
+    path('', views.home, name="home"),
     path("admin/", admin.site.urls),
 ]
